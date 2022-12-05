b345a9540ccd61774f50bb0f9ff80ae41a444d3c
guilherme-gjv <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>
2022-12-04 23:04:52 -0300

    fix: then catch switched to async/await & try catch

--- controllers/Usuario.js
+++ controllers/Usuario.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("../models/Usuario");
 const Usuario = mongoose.model("usuarios");
 
-const userCreate = (req, res) => {
+const userCreate = async (req, res) => {
   const usuario = new Usuario({
     name: req.body.name,
     email: req.body.email,
@@ -11,86 +11,72 @@ const userCreate = (req, res) => {
     isMonitor: req.body.isMonitor,
     password: req.body.password,
   });
-  usuario
-    .save()
-    .then(() => {
-      console.log("usuario salvo :3");
-      return res.status(200).json({ message: "User saved." });
-    })
-    .catch((error) => {
-      return res.status(500).json({ message: "User not created: " + error });
-    });
+  try {
+    await usuario.save();
+    return res.status(200).json({ message: "User saved." });
+  } catch (error) {
+    return res.status(500).json({ message: "User not created: " + error });
+  }
 };
 
-const userFindAll = (req, res) => {
-  Usuario.find()
-    .lean()
-    .then((usuarios) => {
-      return res.status(200).json(usuarios);
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: "Users not found: " + error });
-    });
+const userFindAll = async (req, res) => {
+  try {
+    const users = await Usuario.find().lean();
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(404).json({ message: "Users not found: " + error });
+  }
 };
 
-const userFindById = (req, res) => {
-  Usuario.findOne({ _id: req.params.id })
-    .lean()
-    .then((usuario) => {
-      console.log(usuario);
-      return res.status(200).json(usuario);
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: "User not found: " + error });
-    });
+const userFindById = async (req, res) => {
+  try {
+    const user = await Usuario.findOne({ _id: req.params.id }).lean();
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(404).json({ message: "User not found: " + error });
+  }
 };
 
-const userFindByEnrolmentCode = (req, res) => {
-  Usuario.findOne({ enrolmentCode: req.params.enrolmentCode })
-    .lean()
-    .then((usuario) => {
-      console.log(usuario);
-      return res.status(200).json(usuario);
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: "User not found: " + error });
-    });
+const userFindByEnrolmentCode = async (req, res) => {
+  try {
+    const user = await Usuario.findOne({
+      enrolmentCode: req.params.enrolmentCode,
+    }).lean();
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(404).json({ message: "User not found: " + error });
+  }
 };
 
-const userEdit = (req, res) => {
-  Usuario.findOne({ _id: req.body.id })
-    .then((usuario) => {
-      usuario.name = req.body.name;
-      usuario.email = req.body.email;
-      usuario.enrolmentCode = req.body.enrolmentCode;
-      usuario.isAdmin = req.body.isAdmin;
-      usuario.isMonitor = req.body.isMonitor;
-      usuario.password = req.body.password;
-      usuario
-        .save()
-        .then(() => {
-          return res.status(200).json({ message: "User edited." });
-        })
-        .catch((err) => {
-          return res.status(500).json({ message: "User could not be edited." });
-        });
-    })
-    .catch((error) => {
-      return res
-        .status(404)
-        .json({ message: "User not found to edit: " + error });
-    });
+const userEdit = async (req, res) => {
+  try {
+    const user = await Usuario.findOne({ _id: req.body.id });
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.enrolmentCode = req.body.enrolmentCode;
+    user.isAdmin = req.body.isAdmin;
+    user.isMonitor = req.body.isMonitor;
+    user.password = req.body.password;
+    try {
+      await user.save();
+      return res.status(200).json({ message: "User edited." });
+    } catch (error) {
+      return res.status(500).json({ message: "User could not be edited." });
+    }
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ message: "User not found to edit: " + error });
+  }
 };
 
-const userDelete = (req, res) => {
-  Usuario.deleteOne({ _id: req.body.id })
-    .lean()
-    .then(() => {
-      res.status(200).json({ message: "deleted user" });
-    })
-    .catch((err) => {
-      console.log("erro: " + err);
-    });
+const userDelete = async (req, res) => {
+  try {
+    await Usuario.deleteOne({ _id: req.body.id }).lean();
+    return res.status(200).json({ message: "Deleted user." });
+  } catch (error) {
+    return res.status(404).json({ message: "Could not delete user: " + error });
+  }
 };
 
 module.exports = {
